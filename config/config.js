758d6d9cd8784dcd5536f7c282fe76bf78f06a1d
shuh <nonya@biz.com>
2019-08-22 19:10:04 -0400

    updated for heroku

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ module.exports = {
     "development": {
         "username": "root",
         "password": "root",
-        "database": "burger_db",
+        "database": "burgers_db",
         "host": "127.0.0.1",
         "dialect": "mysql"
     },
