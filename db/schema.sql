3849cf48bef332ea1a99403050009cd8f64e3b77
shuh <nonya@biz.com>
2019-08-22 17:32:18 -0400

    updated for heroku

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,13 @@
-### Schema
-DROP DATABASE IF EXISTS burger_db;
-CREATE DATABASE burger_db;
-USE burger_db;
+CREATE DATABASE IF NOT EXISTS burgers_db;
+USE burgers_db;
 
-CREATE TABLE burgers
-(
-	id int NOT NULL AUTO_INCREMENT,
-	BurgerOrder varchar(255) NOT NULL,
-	Devour BOOLEAN DEFAULT false,
-	PRIMARY KEY (id)
+-- If the table already exists, remove it before trying to create the table again
+DROP TABLE IF EXISTS burgers;
+
+-- Create the burgers table
+CREATE TABLE burgers (
+    id int NOT NULL AUTO_INCREMENT,
+    burger_name varchar(255) NOT NULL,
+    devoured BOOL DEFAULT false,
+    PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,3 @@
-INSERT INTO burgers (burgerOrder) VALUES ('double cheeseburger');
-INSERT INTO burgers (burgerOrder) VALUES ('hamburger');
-INSERT INTO burgers (burgerOrder, devour) VALUES ('hot dog', true);
-INSERT INTO burgers (burgerOrder, devour) VALUES ('bacon cheeseburger', true);
-INSERT INTO burgers (burgerOrder, devour) VALUES ('southwestern', true);
-INSERT INTO burgers (burgerOrder) VALUES ('taco burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', FALSE);
