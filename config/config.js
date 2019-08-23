d6d545570a726d46dcc875857f85521a905af335
shuh <nonya@biz.com>
2019-08-23 14:58:49 -0400

    skeleton

--- config/config.js
+++ config/config.js
@@ -1,22 +1,26 @@
-module.exports = {
-    "development": {
-        "username": "root",
-        "password": "root",
-        "database": "burgers_db",
-        "host": "127.0.0.1",
-        "dialect": "mysql"
-    },
-
-    "test": {
-        "username": "root",
-        "password": null,
-        "database": "database_test",
-        "host": "127.0.0.1",
-        "dialect": "mysql"
-    },
-
-    "production": {
-        "use_env_variable": "JAWSDB_mysql://vl48dah3y4mt1342:ouqp7v1pvxfzhmdj@rtzsaka6vivj2zp1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/cpcqzvkjw0zz9aat",
-        "dialect": "mysql"
-    }
+{
+  "development": {
+    "username": "root",
+    "password": null,
+    "database": "database_development",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
+  }
 }
