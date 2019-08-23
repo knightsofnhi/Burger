68309e1384f93987931888c4dfb463f53fcf0792
shuh <nonya@biz.com>
2019-08-23 12:52:58 -0400

    working burger

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   host: "localhost",
-  port: process.env.PORT || 8080,
+  port: process.env.PORT || 3306,
   user: "root",
   password: "root",
   database: "burgers_db"
